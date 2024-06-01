2eb1da31ec7d0869c2cbe29390c0df879fe6f70b
Mingjing Sun <mjsun.sjtu@gmail.com>
2024-06-01 14:45:49 +0200

    l

--- slurm.sh
+++ slurm.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -l
 
 #SBATCH -J dspoddfl
-#SBATCH --time=0-5:00:00
-#SBATCH --array=0-5
+#SBATCH --time=0-6:00:00
+#SBATCH --array=0-107
 #SBATCH -p epyc2
 #SBATCH -o outfiles/%A_%a.out
 
 COMMAND='source /storage/homefs/ms23h901/git/DPFL_1/.venv/bin/activate'
 echo $COMMAND
 
-COMMAND="python main.py $SLURM_ARRAY_TASK_ID DPFL"
+COMMAND="python main.py $SLURM_ARRAY_TASK_ID"
 echo $COMMAND
 
 $COMMAND
